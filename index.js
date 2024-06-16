84a5e6baa4dedca450737c7721af24189b324454
ANUSHKA ARORA <131133233+Shiningstar2501@users.noreply.github.com>
2024-06-16 14:14:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const love=document.getElementById("hello");
+const name1=document.getElementById("name1");
+const name2=document.getElementById("name2");
+
+
+
+// function love_cal(){
+//     prompt("YOur NAme ");
+//      prompt("Enter the name of your crush");
+//     var num= math.random()*100;
+//     var num2= math.floor(num)+1;
+//     document.write("The love between" +name1+"and "+name2 +"is"+num2);
+
+// }
+function LOVE_CAL(){
+    
+}
